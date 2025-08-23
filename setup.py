b97dc21ea44f2f4f921374c397ba0d07e41af4cd
SBAK729 <sench729@gmail.com>
2025-08-23 17:07:56 +0300

    feat: setup local packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    namae="DirectEd Turoring Bot",
+    version="0.0.0",
+    author="Sintayehu Bikila and Karanja Kariuki",
+    author_email="sench729@gmail.com,kkariuki2007@gmail.com",
+    packages= find_packages(),
+    insall_requires = []
+)
